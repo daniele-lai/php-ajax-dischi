e9c556f94b4e3aafc9953fafaaa7e6d078047cc0
Daniele <danielelai86@gmail.com>
2021-05-05 15:51:22 +0200

    Aggiunta parte prima milestone

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php 
+    include __DIR__ . "/api.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Google Font -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+    <header>
+      <img src="img/download.png" alt="Logo Spotify">
+    </header>
+    <div class="container">
+      <div class="wrapper">
+        <!-- <div v-if="changeGenre == album.genre || changeGenre == ''" class="card" v-for="album in albums">
+          <img :src="album.poster" alt="album.title">
+          <h2>{{album.title}}</h2>
+          <h3>{{album.author}}</h3>
+          <span>{{album.genre}}</span>
+          <span>{{album.year}}</span>
+        </div> -->
+        <?php foreach ( $albums as $album ) { ?>
+            <div class="card">
+                <img src="<?php echo $album["poster"] ?>" alt="<?php echo $album["title"]; ?>">
+                <h2>
+                    <?php echo $album["title"]; ?>
+                </h2>
+                <h3>
+                    <?php echo $album["author"]; ?>  
+                </h3>
+                <span>
+                    <?php echo $album["genre"]; ?>  
+                </span>
+                <span>
+                    <?php echo $album["year"]; ?>  
+                </span>
+            </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
